400bfa882837a382f763f271f6eb3d9436d7b3e4
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-03-15 10:36:05 +0800

    移动文件,修改名称

---
+++ README.txt
@@ -0,0 +1,1 @@
+使用游览器打开 goahead/LINUX/html/index.html 文件以阅读安装和使用说明
--- package-make/mkpkg.sh
+++ package-make/mkpkg.sh
@@ -1,5 +1,25 @@
-#!/bin sh
+#!/bin/sh
 #两个目录中文件都是符号链接,通过连接到不同配置文件打包不同版本的包.
 #使用时需要将其中的符号链接指向对应的文件(目录).
-tar --exclude=".*"  -phcvf webs-binary-hl3104jd.tar webs-binary-hl3104jd
-tar --exclude=".*"  -phcvf webs-binary-hl3104ha.tar webs-binary-hl3104ha
+tar --exclude=".*" \
+	--exclude="trace.txt" \
+	--exclude="*.md" \
+	--exclude="log.txt" \
+	--exclude="version-template.js" \
+	--exclude="docs/*" \
+	--exclude="matrixssl/*" \
+	--exclude="over/*" \
+	--exclude="asptest.asp" \
+	-phcvf webs-binary-hl3104jd.tar -C webs-binary-hl3104jd mnt
+
+
+tar --exclude=".*" \
+	--exclude="trace.txt" \
+	--exclude="*.md" \
+	--exclude="log.txt" \
+	--exclude="version-template.js" \
+	--exclude="docs/*" \
+	--exclude="matrixssl/*" \
+	--exclude="over/*" \
+	--exclude="asptest.asp" \
+	-phcvf webs-binary-hl3104ha.tar -C webs-binary-hl3104ha mnt
