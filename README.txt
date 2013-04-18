afc47f1e2d3e7c963a25581693a5778b84122849
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-04-18 16:54:47 +0800

    方便整个项目的配置,生成一些符号链接结构,make目标

--- README.txt
+++ README.txt
@@ -1,1 +1,24 @@
+webs项目
+* goahead 服务器端代码,c语言编写
+* www 客户端代码,html css js
+* package-make 生成二进制包,用于web在线升级或者使用ftp上传升级
+* out 输出目录
+
+gohead主目录:/home/zodiac1111/workspace/goahead
+www: /home/zodiac1111/Aptana Studio 3 Workspace/www
+package-make: /home/zodiac1111/workspace/package-make
+
 使用游览器打开 goahead/LINUX/html/index.html 文件以阅读安装和使用说明
+
+文件结构(tree):
+.
+|-- goahead -> /home/zodiac1111/workspace/goahead
+|-- out
+|-- package-make
+|-- README.txt
+`-- www -> /home/zodiac1111/Aptana\ Studio\ 3\ Workspace/www
+
+**注意**
+goahead 和 www 都是符号链接到真正的目录.
+    Makefile中两个变量用于方便的建立符号链接,参见Makefile
+如果他们是真正的目录,则不用改变了.
--- package-make/mkpkg.sh
+++ package-make/mkpkg.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 #两个目录中文件都是符号链接,通过连接到不同配置文件打包不同版本的包.
 #使用时需要将其中的符号链接指向对应的文件(目录).
+
+#webs-binary-hl3104jd 文件夹是一个完整的终端的文件结构
+#使用  tree webs-binary-hl3104jd 命令查看文件结构,如下:
+#webs-binary-hl3104jd/
+#`-- mnt
+#    `-- nor
+#        |-- bin
+#        |   |-- webs -> /home/zodiac1111/workspace/goahead/LINUX/webs
+#        |   `-- webs.update -> /home/zodiac1111/workspace/goahead/LINUX/webs.update
+#        |-- conf
+#        |   |-- goahead.conf -> /home/zodiac1111/workspace/goahead/conf/goahead.conf
+#        |   `-- monparam_name.conf -> /home/zodiac1111/workspace/goahead/conf/monparam_name_JD.conf
+#        `-- www -> /home/zodiac1111/Aptana\ Studio\ 3\ Workspace/www
+#
+#  **注意** 文件和目录都是通过符号链接指向实际的文件和目录,这样tar可以
+# 追随符号链接只真正的文件和目录,并且把他打包进来.
+
 tar --exclude=".*" \
 	--exclude="trace.txt" \
 	--exclude="*.md" \
@@ -11,7 +28,7 @@ tar --exclude=".*" \
 	--exclude="over*" \
 	--exclude="asptest.asp" \
 	--exclude="makeversion.sh" \
-	-phcvf webs-binary-hl3104jd.tar -C webs-binary-hl3104jd mnt
+	-phzcvf ../out/webs-binary-hl3104jd.tar.gz -C webs-binary-hl3104jd mnt
 
 
 tar --exclude=".*" \
@@ -24,4 +41,8 @@ tar --exclude=".*" \
 	--exclude="over*" \
 	--exclude="asptest.asp" \
 	--exclude="makeversion.sh" \
-	-phcvf webs-binary-hl3104ha.tar -C webs-binary-hl3104ha mnt
+	-phzcvf ../out/webs-binary-hl3104ha.tar.gz -C webs-binary-hl3104ha mnt
+
+#自动传输
+#lftp -u anonymous,holley 192.168.1.151\
+#	-e "put webs-binary-hl3104ha.tar.gz -o nor/ ;quit" 
